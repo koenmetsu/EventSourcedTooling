23a61415a1298959097acaf9f501aebfc412477d
Michel Grootjans <michel.grootjans@gmail.com>
2018-01-30 15:20:01 +0100

    cleanup

--- EventSourcedTooling/CustomerStartedShoppingHandler.cs
+++ EventSourcedTooling/CustomerStartedShoppingHandler.cs
@@ -2,7 +2,7 @@
 {
     public class CustomerStartedShoppingHandler : ICommandHandler<StartShopping>
     {
-        public IEvent Handle(StartShopping command)
+        public object Handle(StartShopping command)
         {
             //return new CustomerStartedShopping(command.CustomerId, command.CartId);
             return null;
