2cc4055b835e1efbe1f24bb9328d120b2b2f0358
koen.metsu <koen.metsu@gmail.com>
2018-01-30 17:11:46 +0100

    Generate html report of tests that ran

--- EventSourcedTooling/Generated/CustomerPlacedOrder.cs
+++ EventSourcedTooling/Generated/CustomerPlacedOrder.cs
@@ -1,28 +1,33 @@
 using System.Collections.Generic;
 
-namespace EventSourcedTooling {
-	public struct CustomerPlacedOrder : IEvent {
-public CustomerPlacedOrder(string CustomerId, string CartId, List<Product> Products, string OrderedAt){
-this.CustomerId = CustomerId;
-this.CartId = CartId;
-this.Products = Products;
-this.OrderedAt = OrderedAt;
-}
-		public string CustomerId { get; set; }
-		public string CartId { get; set; }
-		public List<Product> Products { get; set; }
-		public string OrderedAt { get; set; }
-	}
-	public struct Product : IEvent {
-public Product(string SKU, string Quantity, string PriceInCents, string Currency){
-this.SKU = SKU;
-this.Quantity = Quantity;
-this.PriceInCents = PriceInCents;
-this.Currency = Currency;
-}
-		public string SKU { get; set; }
-		public string Quantity { get; set; }
-		public string PriceInCents { get; set; }
-		public string Currency { get; set; }
-	}
+namespace EventSourcedTooling
+{
+    public struct CustomerPlacedOrder : IEvent
+    {
+        public CustomerPlacedOrder(string CustomerId, string CartId, List<Product> Products, string OrderedAt)
+        {
+            this.CustomerId = CustomerId;
+            this.CartId = CartId;
+            this.Products = Products;
+            this.OrderedAt = OrderedAt;
+        }
+        public string CustomerId { get; set; }
+        public string CartId { get; set; }
+        public List<Product> Products { get; set; }
+        public string OrderedAt { get; set; }
+    }
+    public struct Product : IEvent
+    {
+        public Product(string SKU, string Quantity, string PriceInCents, string Currency)
+        {
+            this.SKU = SKU;
+            this.Quantity = Quantity;
+            this.PriceInCents = PriceInCents;
+            this.Currency = Currency;
+        }
+        public string SKU { get; set; }
+        public string Quantity { get; set; }
+        public string PriceInCents { get; set; }
+        public string Currency { get; set; }
+    }
 }
