da25f727d50f8b94585e957e71b39734d1f2c0b0
koen.metsu <koen.metsu@gmail.com>
2018-01-30 11:34:39 +0100

    Hello world

---
+++ EventSourcedTooling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventSourcedTooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
